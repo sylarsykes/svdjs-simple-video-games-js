f89f25a91851b3e6255b7f9b4cf1356e8016e0f1
Juan <sylar.sykes@gmail.com>
2020-04-24 16:05:18 +0200

    #0 | Configure environment
    Resume:
    https://trello.com/c/Amc2cdia

---
+++ react-js-memory-game/webpack.config.js
@@ -0,0 +1,67 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+  UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
+  path = require('path')
+
+const environment = process.env.NODE_ENV || 'development'
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry:  [path.join(path.join(__dirname, 'src'), 'index.js')],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    inline: true,
+    watchContentBase: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    // historyApiFallback: true,
+    open: true,
+    port: 3001,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  module: {
+    rules: [
+      // https://webpack.js.org/loaders/svg-inline-loader/
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      },
+      // https://webpack.js.org/loaders/css-loader/
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: environment === 'development',
+            },
+          },
+          'css-loader',
+          'postcss-loader',
+        ]
+      },
+      // https://webpack.js.org/loaders/babel-loader/
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      { test: /\.js$/, use: ['source-map-loader'], enforce: 'pre'}
+    ]
+  },
+  node: {
+    console: true,
+	  __dirname: true,
+  },
+  plugins:[
+    new HtmlWebPackPlugin({template: './public/index.html', filename: 'index.html'}),
+    new MiniCssExtractPlugin({template: './public/css/styles.css', filename: 'styles.css'}),
+    new UglifyJsPlugin()
+  ]
+}
